12e5c91576176ab443d58121775725f09a78e33f
Bennie <brobinson88@gmail.com>
2017-08-18 10:32:21 -0400

    Add more foods

--- src/index.js
+++ src/index.js
@@ -16,9 +16,49 @@ const menu = [{
   price: 1.5,
   isDrink: true
 },{
+  menuNumber: 2,
+  description: 'Green Tea',
+  price: 1.9,
+  isDrink: true
+},{
+  menuNumber: 3,
+  description: 'Freshly Ground Coffee',
+  price: 2,
+  isDrink: true
+},{
+  menuNumber: 4,
+  description: 'Czech Pilsner',
+  price: 3.5,
+  isDrink: true
+},{
+  menuNumber: 5,
+  description: 'Yeti Stout',
+  price: 4.5,
+  isDrink: true
+},{
   menuNumber: 10,
   description: 'Mushroom and Bacon Pasta',
   price: 6,
+  isDrink: false
+},{
+  menuNumber: 11,
+  description: 'Chili Con Carne',
+  price: 7.5,
+  isDrink: false
+},{
+  menuNumber: 12,
+  description: 'Borsch With Smetana',
+  price: 4.5,
+  isDrink: false
+}, {
+  menuNumber: 13,
+  description: 'Lamb Skewers with Tatziki',
+  price: 8,
+  isDrink: false
+},{
+  menuNumber: 14,
+  description: 'Beef Stroganoff',
+  price: 8.5,
   isDrink: false
 }]
 
